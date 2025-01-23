0d26ffca35000c63ea5420d5105de7ffb038c624
ecuche <ucheemmanuel1@gmail.com>
2025-01-23 14:23:25 +0100

    hovertext function added

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
+
+    $('body').append('<div id="hoverdiv"></div>');
+    $('#hoverdiv').css({'display':'none', 'position':'absolute', 'font-size': '14px', 'background-color': '#fff', 'color': '#404040', 'border': '1px solid #999', 'padding': '7px' });
+    $(document).on('mousemove', '[hovertext]', function(e){
+        var text = $(this).attr('hovertext');
+        $('#hoverdiv').text(text).show();
+        $('#hoverdiv').css({'top': e.clientY + 10, 'left': e.clientX + 10});
+    });
+    $(document).on('mouseout', '[hovertext]', function(e){
+        $('#hoverdiv').hide();
+    });
+    
+
+    
     home_url = $('#home_url').attr('href')
 
     $(document).on('click', '.zoomable-image', function () {
